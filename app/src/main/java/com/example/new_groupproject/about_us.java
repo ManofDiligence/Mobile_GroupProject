59174dc56b0c281dfa8527c0b959c2e412598e97
chiwangso <chiwang1712@gmail.com>
2023-04-15 19:48:38 +0800

    small changes

--- app/src/main/java/com/example/new_groupproject/about_us.java
+++ app/src/main/java/com/example/new_groupproject/about_us.java
@@ -3,13 +3,44 @@ package com.example.new_groupproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
 
 public class about_us extends AppCompatActivity {
+
+    private TextView about_title;
+    private float dX, dY;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
 
+        about_title = findViewById(R.id.about_title);
+
+        about_title.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getActionMasked()) {
+                    case MotionEvent.ACTION_DOWN:
+                        dX = v.getX() - event.getRawX();
+                        dY = v.getY() - event.getRawY();
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        v.animate()
+                                .x(event.getRawX() + dX)
+                                .y(event.getRawY() + dY)
+                                .setDuration(0)
+                                .start();
+                        break;
+                    default:
+                        return false;
+                }
+                return true;
+            }
+        });
     }
+
+
 }
