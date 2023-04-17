800552c15689d262279b541554964e26b46ea178
Vincent <chiwang1712@gmail.com>
2023-04-17 20:04:49 +0800

    add link to about us

--- app/src/main/java/com/example/new_groupproject/about_us.java
+++ app/src/main/java/com/example/new_groupproject/about_us.java
@@ -2,14 +2,23 @@ package com.example.new_groupproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.net.URI;
 
 public class about_us extends AppCompatActivity {
 
+    public TextView intro;
     private TextView about_title;
+    public Button linkToGithub;
     private float dX, dY;
 
     @Override
@@ -17,14 +26,32 @@ public class about_us extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
 
+        intro = findViewById(R.id.intro);
+        linkToGithub = (Button) findViewById(R.id.linkToGithub);
         about_title = findViewById(R.id.about_title);
 
+        //intro.setText("");
+        linkToGithub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                    Toast.makeText(about_us.this, "Clicked", Toast.LENGTH_SHORT).show();
+                    Log.d("Vincent", "onClick: clicked!");
+                    Intent i = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse("https://github.com/ManofDiligence/Mobile_GroupProject"));
+                    startActivity(i);
+
+
+            }
+        });
         // Apply onTouchListener to about_title TextView
         about_title.setOnTouchListener(onTouchListener);
 
         // Add more TextView elements and apply onTouchListener to them as needed
         //pull again
     }
+
+
+
 
     private View.OnTouchListener onTouchListener = new View.OnTouchListener() {
         @Override
